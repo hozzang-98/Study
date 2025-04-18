31d829223aee199d3ef67a75dd2f0eca885c7bf6
hozzang-98 <116536911+hozzang-98@users.noreply.github.com>
2025-04-18 14:05:40 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.06 ms, Memory: 9.52 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -0,0 +1,47 @@
+def time_change(hhmm):
+    
+    mins ,secs = hhmm.split(':')
+    
+    s = int(mins) * 60 + int(secs)
+    
+    return s
+
+def in_opening(pos, op_start, op_end):
+    
+    if op_start <= pos <= op_end:
+            
+        pos = op_end 
+
+    return pos
+
+def solution(video_len, pos, op_start, op_end, commands):
+    
+    answer = ''
+    
+    command_dic = {"next":10, "prev":-10}
+    
+    pos = time_change(pos)
+    op_start = time_change(op_start)
+    op_end = time_change(op_end)
+    video_len = time_change(video_len)
+
+    for command in commands:
+        
+        pos = in_opening(pos, op_start, op_end)
+        
+        pos += command_dic[command]
+
+        if pos <= 0: pos = 0
+        
+        elif pos >= video_len: pos = video_len
+        
+        pos = in_opening(pos, op_start, op_end)
+        
+        
+
+        
+    m = str(pos // 60) if len(str(pos // 60)) == 2 else '0' + str(pos // 60)
+    s = str(pos % 60) if len(str(pos % 60)) == 2 else '0' + str(pos % 60)
+        
+    
+    return '{}:{}'.format(m,s)

17d39b8937720cee2a0a8b77e09473e54bc82222
hozzang-98 <116536911+hozzang-98@users.noreply.github.com>
2025-05-04 23:46:30 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.17 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,24 +1,14 @@
-def solution(survey, choices):
-    answer = ''
-    dic = {'R':0, 'T':0, 'C':0, 'F':0,'J':0,'M':0, 'A':0, 'N':0}
-    
-    for sv, choice in zip(survey,choices):
-        
-        if choice > 4: # 동의 구간
-            dic[sv[1]] += choice - 4
-        else: # 비동의 구간
-            dic[sv[0]] += 4 - choice
+from collections import defaultdict
 
-    if dic['R'] >= dic['T']: answer += 'R'
-    else: answer += 'T'
-        
-    if dic['C'] >= dic['F']: answer += 'C'
-    else: answer += 'F'
+def solution(survey, choices):
     
-    if dic['J'] >= dic['M']: answer += 'J'
-    else: answer += 'M'
-    
-    if dic['A'] >= dic['N']: answer += 'A'
-    else: answer += 'N'
-    
-    return answer
+    score = defaultdict(int)
+    types = [('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N')]
+
+    for sv, ch in zip(survey, choices):
+        if ch < 4:
+            score[sv[0]] += 4 - ch
+        elif ch > 4:
+            score[sv[1]] += ch - 4
+
+    return ''.join(a if score[a] >= score[b] else b for a, b in types)

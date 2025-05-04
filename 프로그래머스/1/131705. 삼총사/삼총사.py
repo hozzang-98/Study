41eaf3c7be22c14025c286318fd33b89fd8d8a55
hozzang-98 <116536911+hozzang-98@users.noreply.github.com>
2025-05-04 23:47:46 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -2,8 +2,6 @@ from itertools import combinations
 
 def solution(numbers):
 
-    number = [-2,3,0,2,-5]
-        
     answer = 0
     tmp_set = list(combinations(numbers,3))
     for s in tmp_set:

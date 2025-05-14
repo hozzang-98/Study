61988f1efb8584533465e4766bbc26813de8c913
hozzang-98 <ghwls1210@naver.com>
2025-05-14 14:07:52 +0900

    SQL: add new tips problems

--- SolveSQL/TIPS/tips2.sql
+++ SolveSQL/TIPS/tips2.sql
@@ -9,3 +9,17 @@ WITH daily_sum as (
 
 SELECT day, max(tip_daily) as tip_daily
 FROM daily_sum;
+
+SELECT * 
+FROM tips
+WHERE total_bill > 
+      (SELECT avg(total_bill) 
+       FROM tips);
+
+WITH day_sales as (
+  SELECT day, sum(total_bill) as day_sum
+  FROM tips
+  GROUP BY day)
+
+SELECT round(avg(day_sum), 2) as avg_sales
+FROM day_sales;

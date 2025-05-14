8f85b6c4aa184de6d222f4bd558a328d4bec4957
hozzang-98 <ghwls1210@naver.com>
2025-05-14 13:42:57 +0900

    SQL: add new tips problem

--- SolveSQL/TIPS/tips2.sql
+++ SolveSQL/TIPS/tips2.sql
@@ -1,3 +1,11 @@
 SELECT *
 FROM tips
-WHERE size % 2 = 1
+WHERE size % 2 = 1;
+
+WITH daily_sum as (
+  SELECT day, round(sum(tip),2) as tip_daily
+  FROM tips
+  GROUP BY day)
+
+SELECT day, max(tip_daily) as tip_daily
+FROM daily_sum;

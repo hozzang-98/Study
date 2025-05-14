160877e30ee3c315a10c2cdc5d9054ddbf486afd
hozzang-98 <ghwls1210@naver.com>
2025-05-14 13:49:47 +0900

    SQL: add new olist_order_items_dataset

--- SolveSQL/olist/olist_order_items_dataset.sql
+++ SolveSQL/olist/olist_order_items_dataset.sql
@@ -1,4 +1,7 @@
 SELECT seller_id, count(DISTINCT order_id) as orders
 FROM olist_order_items_dataset
 GROUP BY seller_id
-HAVING orders >= 100
+HAVING orders >= 100;
+
+SELECT min(date(order_purchase_timestamp)) as first_order_date, max(date(order_purchase_timestamp)) as last_order_date
+FROM olist_orders_dataset

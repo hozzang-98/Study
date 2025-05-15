803c646d38e1ffa3b5a324a0aaad90836d74286c
hozzang-98 <ghwls1210@naver.com>
2025-05-15 17:35:41 +0900

    SQL: modify file name

--- SolveSQL/TIPS/average_sales.sql
+++ SolveSQL/TIPS/average_sales.sql
@@ -1,21 +1,3 @@
-SELECT *
-FROM tips
-WHERE size % 2 = 1;
-
-WITH daily_sum as (
-  SELECT day, round(sum(tip),2) as tip_daily
-  FROM tips
-  GROUP BY day)
-
-SELECT day, max(tip_daily) as tip_daily
-FROM daily_sum;
-
-SELECT * 
-FROM tips
-WHERE total_bill > 
-      (SELECT avg(total_bill) 
-       FROM tips);
-
 WITH day_sales as (
   SELECT day, sum(total_bill) as day_sum
   FROM tips
--- SolveSQL/TIPS/high_bill.sql
+++ SolveSQL/TIPS/high_bill.sql
@@ -2,12 +2,4 @@ SELECT *
 FROM tips
 WHERE total_bill > 
       (SELECT avg(total_bill) 
-       FROM tips);
-
-WITH day_sales as (
-  SELECT day, sum(total_bill) as day_sum
-  FROM tips
-  GROUP BY day)
-
-SELECT round(avg(day_sum), 2) as avg_sales
-FROM day_sales;
+       FROM tips)

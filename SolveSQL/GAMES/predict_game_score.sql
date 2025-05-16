66fb24987b889e13c2ec1c1ec91f35536bcbbb64
hozzang-98 <ghwls1210@naver.com>
2025-05-16 18:16:22 +0900

    SQL: add new solution about game problem

--- SolveSQL/GAMES/predict_game_score.sql
+++ SolveSQL/GAMES/predict_game_score.sql
@@ -52,4 +52,29 @@ WHERE
     T1.critic_score is null
     or T1.user_score is null
   )
-  and T1.year >= 2015
+  and T1.year >= 2015;
+
+# 다른 풀이
+
+  WITH genre_avg AS (
+  SELECT
+    genre_id,
+    ROUND(AVG(critic_score), 3) AS avg_critic_score,
+    CEIL(AVG(critic_count)) AS avg_critic_count,
+    ROUND(AVG(user_score), 3) AS avg_user_score,
+    CEIL(AVG(user_count)) AS avg_user_count
+  FROM games
+  GROUP BY genre_id
+)
+
+SELECT
+  g.game_id,
+  g.name,
+  COALESCE(g.critic_score, ga.avg_critic_score) AS critic_score,
+  COALESCE(g.critic_count, ga.avg_critic_count) AS critic_count,
+  COALESCE(g.user_score, ga.avg_user_score) AS user_score,
+  COALESCE(g.user_count, ga.avg_user_count) AS user_count
+FROM games g
+LEFT JOIN genre_avg ga ON g.genre_id = ga.genre_id
+WHERE (g.critic_score IS NULL OR g.user_score IS NULL)
+  AND g.year >= 2015;

418739f86b8ff5a046b0511dcd1c28bbea6fa777
hozzang-98 <116536911+hozzang-98@users.noreply.github.com>
2025-05-29 17:58:54 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 46.94 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 22.57 ms
+메모리: 9.23 MB, 시간: 46.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 28일 23:50:08
+2025년 05월 29일 17:58:51
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,30 +1,23 @@
 from collections import deque
 
 def solution(board):
-    def bfs(start):
-        direc = {0:[-1, 0], 1:[0, 1], 2:[1, 0], 3:[0, -1]} # 북,동,남,서 순서
-        length = len(board)
-        visited = [[987654321]*length for _ in range(length)]
-        visited[0][0] = 0
+    N = len(board)
+    directions = [(-1,0), (0,1), (1,0), (0,-1)]  # 상우하좌
+    visited = [[[float('inf')] * 4 for _ in range(N)] for _ in range(N)]
+    q = deque()
 
-        q = deque([start]) # x, y, cost, dir
-        while q:
-            x, y, cost, d = q.popleft()
-            for i in range(4): # 북,동,남,서 순서
-                nx = x + direc[i][0]
-                ny = y + direc[i][1]
+    for d in [1, 2]:  # 오른쪽, 아래 시작
+        visited[0][0][d] = 0
+        q.append((0, 0, 0, d))  # x, y, cost, dir
 
-                # board 안에 있고, 벽이 아닌지 확인
-                if 0 <= nx < length and 0 <= ny < length and board[nx][ny] == 0:
-                    
-                    # 비용계산
-                    if i == d : ncost = cost + 100
-                    else : ncost =  cost + 600
-                    # 최소 비용이면 갱신 후 endeque!
-                    if ncost < visited[nx][ny]:
-                        visited[nx][ny] = ncost
-                        q.append([nx, ny, ncost, i])
-                        
-        return visited[-1][-1]
-    
-    return min([bfs((0, 0, 0, 1)), bfs((0, 0, 0, 2))])
+    while q:
+        x, y, cost, dir = q.popleft()
+        for i, (dx, dy) in enumerate(directions):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+                ncost = cost + (100 if i == dir else 600)
+                if ncost < visited[nx][ny][i]:
+                    visited[nx][ny][i] = ncost
+                    q.append((nx, ny, ncost, i))
+
+    return min(visited[N-1][N-1])

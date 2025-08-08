ae43f582b603ee0e8a2fb9951e58094444f4f294
hozzang-98 <ghwls1210@naver.com>
2025-08-08 19:46:05 +0900

    SQL: Average Time of Process per Machine

---
+++ LeetCode/sql/Average Time of Process per Machine.sql
@@ -0,0 +1,10 @@
+SELECT
+  S.machine_id,
+  ROUND(AVG(E.timestamp - S.timestamp), 3) AS processing_time
+FROM Activity S
+JOIN Activity E
+  ON S.machine_id = E.machine_id
+ AND S.process_id = E.process_id
+WHERE S.activity_type = 'start'
+  AND E.activity_type = 'end'
+GROUP BY S.machine_id;

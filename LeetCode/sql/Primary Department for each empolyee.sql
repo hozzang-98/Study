9d428dd040cc671e0c1889d53f80e03d7df60487
hozzang-98 <ghwls1210@naver.com>
2025-08-24 20:04:51 +0900

    SQL: Primary department for each empolyee

---
+++ LeetCode/sql/Primary Department for each empolyee.sql
@@ -0,0 +1,9 @@
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+    or employee_id in (SELECT employee_id
+                        FROM Employee
+                        GROUP BY employee_id
+                        HAVING COUNT(department_id) = 1)
